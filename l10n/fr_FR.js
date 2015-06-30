45e6d48d7b738c590802021f751cd649e7bd5b80
Pascal SALAUN <pascal.salaun@laposte.net>
2015-06-30 09:10:26 +0200

    Create fr_FR.js

---
+++ l10n/fr_FR.js
@@ -0,0 +1,21 @@
+OC.L10N.register(
+"files_gpxviewer_extended",
+{
+'Close' : 'Fin',
+'Author' : 'Auteur',
+'Description' : 'Description',
+'Date' : 'Date',
+'Start' : 'D&eacute;but',
+'Duration' : 'Dur&eacute;e',
+'Distance' : 'Distance',
+'Pace' : 'Rythme',
+'Heartrate' : 'Pulsation',
+'Total Ascend' : 'Monteacute;ee',
+'Height' : 'Altitude,
+'Lap times' : 'Temps aux tours',
+'Lap time' : 'Temps au tour',
+'Time' : 'Temps',
+'Speed' : 'Vitesse;',
+'Kilometer': 'Kilom&egrave;tre'
+},
+"nplurals=2; plural=(n != 1);");
